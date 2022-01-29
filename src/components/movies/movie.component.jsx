13174f2b8d7819cf0b032dc9adfcb4ecd2f2a3ee
Samir Prakash <Samir.Prakash@gmail.com>
2022-01-29 13:33:05 -0600

    sample movie disply

--- src/components/movies/movie.component.jsx
+++ src/components/movies/movie.component.jsx
@@ -1,9 +1,35 @@
+import { Fragment, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 const Movie = () => {
   const { id } = useParams();
+  const [movie] = useState({ id, title: 'Some Movie', runtime: 150 });
 
-  return <div>Movie {id}</div>;
+  return (
+    <Fragment>
+      <h2>
+        {movie.title} {movie.id}
+      </h2>
+
+      <table className="table table-compact table-striped">
+        <thead></thead>
+        <tbody>
+          <tr>
+            <td>
+              <strong>Title</strong>
+            </td>
+            <td>{movie.title}</td>
+          </tr>
+          <tr>
+            <td>
+              <strong>Runtime</strong>
+            </td>
+            <td>{movie.runtime} minutes</td>
+          </tr>
+        </tbody>
+      </table>
+    </Fragment>
+  );
 };
 
 export default Movie;
