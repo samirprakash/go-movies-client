1105de9160496a2bbe0c666d3b82ad440041e76a
Samir Prakash <Samir.Prakash@gmail.com>
2022-02-01 21:15:27 -0600

    read movies from server

--- src/components/movies/movies.compoment.jsx
+++ src/components/movies/movies.compoment.jsx
@@ -1,28 +1,40 @@
-import { Fragment, useState } from 'react';
+import { Fragment, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
-const initialState = [
-  { id: 1, title: 'The Shawshank Redemption', runtime: 142 },
-  { id: 2, title: 'The Godfather', runtime: 175 },
-  { id: 3, title: 'The Dark Knight', runtime: 135 },
-];
-
 const Movies = () => {
-  const [movies] = useState(initialState);
+  const [movies, setMovies] = useState([]);
+  const [isLoaded, setIsLoaded] = useState(false);
 
-  return (
-    <Fragment>
-      <h2>Choose a movie</h2>
+  useEffect(() => {
+    getMovies();
+  }, []);
 
-      <ul>
-        {movies.map((movie) => (
-          <li key={movie.id}>
-            <Link to={`/movies/${movie.id}`}>{movie.title}</Link>
-          </li>
-        ))}
-      </ul>
-    </Fragment>
-  );
+  const getMovies = async () => {
+    await fetch('http://localhost:4000/v1/movies')
+      .then((response) => response.json())
+      .then((json) => {
+        setMovies(json.movies);
+        setIsLoaded(true);
+      });
+  };
+
+  if (!isLoaded) {
+    return <p>Loading...</p>;
+  } else {
+    return (
+      <Fragment>
+        <h2>Choose a movie</h2>
+
+        <ul>
+          {movies.map((movie) => (
+            <li key={movie.id}>
+              <Link to={`/movies/${movie.id}`}>{movie.title}</Link>
+            </li>
+          ))}
+        </ul>
+      </Fragment>
+    );
+  }
 };
 
 export default Movies;
