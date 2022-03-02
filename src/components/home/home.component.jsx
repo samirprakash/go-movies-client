92bcd6df4e7c353aed4c0a8f4e47272610cce13f
Samir Prakash <Samir.Prakash@gmail.com>
2022-03-01 21:11:58 -0600

    add an image

--- src/components/home/home.component.jsx
+++ src/components/home/home.component.jsx
@@ -1,5 +1,13 @@
+import Ticket from '../../images/movie_tickets.jpg';
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="text-center">
+      <h2>Home</h2>
+      <hr />
+      <img src={Ticket} alt="movie ticket" />
+    </div>
+  );
 };
 
 export default Home;
